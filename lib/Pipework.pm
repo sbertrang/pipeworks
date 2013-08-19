a1a19f3f5760ebe9b6a57cd410f26572c974a9c1
Simon Bertrang <janus@errornet.de>
2013-08-20 01:28:26 +0200

    improve example, write about motivation and what else to look at

--- lib/Pipework.pm
+++ lib/Pipework.pm
@@ -13,12 +13,21 @@ Pipework - Pipeline Processing Framework
 
 =head1 SYNOPSIS
 
-  use Pipework::Service::MyDoc;
-  my $service = Pipework::Service::MyDoc->new;
-  my $message = $service->message( GetBody => {
+  use Pipework::Line::MyDoc;
+  use Pipework::Message::GetBody;
+  
+  my $line = Pipework::Line::MyDoc->new;
+  
+  # same as Pipework::Stage::FetchURL->new( ... )
+  $line->register( 'FetchURL' );
+  $line->register( 'GetDocumentBody' );
+  $line->register( sub { my $msg = shift; warn( "body:\n" . $msg->body ) } );
+  
+  # same as Pipework::Message::GetBody->new( ... )
+  my $message = $line->message( GetBody => {
     url => 'http://localhost/',
   } );
-  my $result = $service->invoke( $message );
+  my $result = $line->process( $message );
 
 =head1 DESCRIPTION
 
@@ -28,6 +37,21 @@ and improve code-reuse though aspect orientation.
 
 It simply allows to define and organize pipelines that process
 messages in multiple steps and return a result.
+
+=head1 HISTORY
+
+Originally the basic idea is inspired by UNIX concepts.
+There was a lot of thinking how to represent a similar model in programming
+without losing important benefits that UNIX provides.
+When Steve Bate wrote a blog post about L<Messaging as a Programming Model|http://eventuallyconsistent.net/2013/08/12/messaging-as-a-programming-model-part-1/>
+things came together, started to make sense and code began to materialize this
+implementation.
+
+=head1 SEE ALSO
+
+L<http://eventuallyconsistent.net/2013/08/12/messaging-as-a-programming-model-part-1/>,
+L<http://eventuallyconsistent.net/2013/08/14/messaging-as-a-programming-model-part-2/>,
+L<http://eventuallyconsistent.net/2013/08/19/messaging-as-a-programming-model-revisited/>
 
 =cut
 
